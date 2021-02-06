6c1df3da4eed37fc72ff6d092adf7565466fcb2b
Alexander Artemenko <svetlyak.40wt@gmail.com>
2021-02-06 22:31:26 +0300

    Use error-output.

--- src/main.lisp
+++ src/main.lisp
@@ -6,27 +6,22 @@
 
 
 (defun main (&rest argv)
-  (let ((*terminal-io323* *error-output*))
-    (unless argv
-      (format *error-output* "Please, provide a system name.~%")
-      (uiop:quit 1))
+  (unless argv
+    (format *error-output* "Please, provide a system name.~%")
+    (uiop:quit 1))
 
-    (log:config :debug :sane2)
+  (log:config :debug :stream *error-output*)
 
-    (let* ((system-name (first argv)))
-      (log:info "Quickloading system ~S" system-name)
-      (ql:quickload system-name
-                    :silent t)
+  (let* ((system-name (first argv)))
+    (log:info "Quickloading system ~S" system-name)
+    (ql:quickload system-name
+                  :silent t)
 
-      (let* ((builder (docs-builder/api:make-builder system-name))
-             (output-dir
-               (docs-builder/api:build builder
-                                       system-name)))
+    (let* ((builder (docs-builder/api:make-builder system-name))
+           (output-dir
+             (docs-builder/api:build builder
+                                     system-name)))
 
-        (when output-dir
-          (format t "~A~%"
-                  output-dir)
-          ;; (when (>= (length argv)
-          ;;           2)
-          ;;   )
-          )))))
+      (when output-dir
+        (format t "~A~%"
+                output-dir)))))
