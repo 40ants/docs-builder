fc7570814caa58aefc1e99d4a1862b685ef50a08
Alexander Artemenko <svetlyak.40wt@gmail.com>
2021-04-15 23:54:05 +0300

    Command line utility now supports ERROR-ON-WARNINGS too.

--- src/main.lisp
+++ src/main.lisp
@@ -1,5 +1,7 @@
 (defpackage #:docs-builder/main
   (:use #:cl)
+  (:import-from #:defmain
+                #:defmain)
   (:import-from #:docs-builder/core)
   (:import-from #:log4cl)
   (:import-from #:alexandria)
@@ -9,31 +11,36 @@
 (in-package docs-builder/main)
 
 
-(defun main (&rest argv)
-  (unless argv
+(defmain main ((error-on-warnings "If documentation will generate any warnings, build will fail. This can be \"true\" or \"false\"."
+                                  :default "true")
+               &rest systems)
+  (unless systems
     (format *error-output* "Please, provide a system name.~%")
     (uiop:quit 1))
 
   (let ((*trace-output* *error-output*)
-        (*terminal-io* *error-output*))
+        (*terminal-io* *error-output*)
+        (error-on-warnings (string-equal error-on-warnings
+                                         "true")))
     
     (log4cl-extras/config:setup
       '(:level :debug
         :appenders ((this-console :layout :plain))))
 
-    (let* ((system-name (first argv)))
+    (let* ((system-name (first systems)))
       (log:info "Quickloading system ~S" system-name)
       (ql:quickload system-name
                     :silent t)
 
       (let* ((output-dir (ignore-errors
                           (with-log-unhandled ()
-                            (docs-builder/core:build system-name)))))
+                            (docs-builder/core:build system-name
+                                                     :error-on-warnings error-on-warnings)))))
         (unless output-dir
           (log:error "Unable to build docs")
           (uiop:quit 1))
 
-        (let ((output-filename (second argv)))
+        (let ((output-filename (second systems)))
           (when output-filename
             (alexandria:with-output-to-file (s output-filename
                                                :if-does-not-exist :create
